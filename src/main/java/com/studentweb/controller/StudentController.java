97ba48ce8ab937d071b8d0e6a570155f4390421b
KhazarIbrahimov <khazaribrahimov@gmail.com>
2023-04-12 01:32:58 +0400

    restful

--- src/main/java/com/studentweb/controller/StudentController.java
+++ src/main/java/com/studentweb/controller/StudentController.java
@@ -2,65 +2,61 @@ package com.studentweb.controller;
 
 import com.studentweb.studentEntitiy.Student;
 import com.studentweb.studentService.StudentService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class StudentController {
 
     private StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
-        super();
         this.studentService = studentService;
     }
 
     @GetMapping("/students")
-    public String listStudents(Model model){
-        model.addAttribute("students",studentService.getAllStudents());
-        return "students";
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getAllStudents();
+        return new ResponseEntity<List<Student>>(students, HttpStatus.OK);
     }
 
-    @GetMapping("/students/new")
-    public String AddStudent(Model model){
-        Student student = new Student();
-        model.addAttribute("student",student);
-        return "add_student";
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id){
+        Student student = studentService.getStudentById(id);
+        return new ResponseEntity<Student>(student,HttpStatus.OK);
     }
 
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") Student student) {
+    public ResponseEntity<Void> saveStudent(@RequestBody Student student, UriComponentsBuilder builder){
         studentService.saveStudent(student);
-        return "redirect:/students";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/students/{id}").buildAndExpand(student.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
 
-    @GetMapping("/students/edit/{id}")
-    public String editStudentForm(@PathVariable Long id,Model model){
-        model.addAttribute("student",studentService.getStudentById(id));
-        return "edit_student";
-    }
-
-    @PostMapping("/students/{id}")
-    public String updateStudent(@PathVariable Long id ,
-        @ModelAttribute("student") Student student,
-        Model model){
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student){
         Student existingStudent = studentService.getStudentById(id);
         existingStudent.setId(student.getId());
         existingStudent.setName(student.getName());
         existingStudent.setSurName(student.getSurName());
 
         studentService.updateStudent(existingStudent);
-        return "redirect:/students";
+        return new ResponseEntity<Student>(existingStudent,HttpStatus.OK);
     }
 
-    @GetMapping("/students/{id}")
-    public String deleteStudent(@PathVariable Long id){
+    @DeleteMapping("/students/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id){
         studentService.deleteStudentById(id);
-        return "redirect:/students";
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
-
 }
